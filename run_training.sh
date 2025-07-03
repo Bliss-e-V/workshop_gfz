1c9ee7849c7b9036e969d78cf1b957f671246e21
optzGuitar <leo.leo11@hotmail.de>
2025-07-03 14:16:04 +0200

    a

--- run_training.sh
+++ run_training.sh
@@ -4,6 +4,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --exclude=head024,head021,head025,head047,head076,head074,head050,head075
 #SBATCH --mem=128G
+#SBATCH --gpus-per-node=1
 #SBATCH --output=logs/%j.out
 
 apptainer run --nv ../oko-ood/oko-ood.sif python train_ssl_cluster.py "$@"
