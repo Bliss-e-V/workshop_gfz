6197376a3d4b42639801be0ac33147d976df122d
optzGuitar <leo.leo11@hotmail.de>
2025-07-03 12:24:15 +0200

    a

--- run_training.sh
+++ run_training.sh
@@ -2,6 +2,7 @@
 #SBATCH --job-name=oko_ood
 #SBATCH --partition=gpu-5h
 #SBATCH --cpus-per-task=8
+#SBATCH --exclude=head024,head021,head025,head047,head076,head074,head050,head075
 #SBATCH --mem=128G
 #SBATCH --output=logs/%j.out
 
