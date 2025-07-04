eb64a2e93f858c9ede0fbc43a110c13318623b0d
optzGuitar <leo.leo11@hotmail.de>
2025-07-04 14:00:26 +0200

    a

--- run_training.sh
+++ run_training.sh
@@ -4,6 +4,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --exclude=head024,head021,head025,head047,head076,head074,head050,head075
 #SBATCH --mem=128G
+#SBATCH --constraint="40gb|80gb|h100"
 #SBATCH --gpus-per-node=1
 #SBATCH --output=logs/%j.out
 
